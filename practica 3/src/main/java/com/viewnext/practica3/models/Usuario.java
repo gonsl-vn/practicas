2708f6ce35a6d1054a3bbfb49365030c3d0104cf
Javier Arias Rodriguez <javier.arias@viewnext.com>
2025-03-05 10:32:14 +0100

    Yaml añadido y usuarios configurados

--- practica 3/src/main/java/com/viewnext/practica3/models/Usuario.java
+++ practica 3/src/main/java/com/viewnext/practica3/models/Usuario.java
@@ -1,14 +1,23 @@
 package com.viewnext.practica3.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Usuario {
-
+    @Column(name = "nombre")
     private String name;
+    @Id
+    @Column(name = "dni", nullable = false, unique = true)
     private String dni;
+    @Column(name = "surname")
     private String surname;
+    @Column(name = "age")
+    private int age;
 
 }
--- practica 3/src/main/java/com/viewnext/practica3/repository/UsersRepository.java
+++ practica 3/src/main/java/com/viewnext/practica3/repository/UsersRepository.java
@@ -14,16 +14,16 @@ public class UsersRepository {
     private static List<Usuario> lUsuarios = new ArrayList<>();
 
     static {
-        lUsuarios.add(new Usuario("Juan Carlos", "12345678A", "García"));
-        lUsuarios.add(new Usuario("María José", "98765432B", "Martínez"));
-        lUsuarios.add(new Usuario("Pedro Luis", "11111111C", "Díaz"));
-        lUsuarios.add(new Usuario("Ana Isabel", "22222222D", "González"));
-        lUsuarios.add(new Usuario("Carlos Alberto", "33333333E", "Rodríguez"));
-        lUsuarios.add(new Usuario("Sofía Elena", "44444444F", "Gómez"));
-        lUsuarios.add(new Usuario("Jorge Luis", "55555555G", "Hernández"));
-        lUsuarios.add(new Usuario("Lucía Sofía", "66666666H", "Sánchez"));
-        lUsuarios.add(new Usuario("Tomás Juan", "77777777I", "García"));
-        lUsuarios.add(new Usuario("Eva María", "88888888J", "Martínez"));
+        lUsuarios.add(new Usuario("Juan Carlos", "12345678A", "García", 20));
+        lUsuarios.add(new Usuario("María José", "98765432B", "Martínez", 80));
+        lUsuarios.add(new Usuario("Pedro Luis", "11111111C", "Díaz", 43));
+        lUsuarios.add(new Usuario("Ana Isabel", "22222222D", "González", 89));
+        lUsuarios.add(new Usuario("Carlos Alberto", "33333333E", "Rodríguez", 54));
+        lUsuarios.add(new Usuario("Sofía Elena", "44444444F", "Gómez", 2));
+        lUsuarios.add(new Usuario("Jorge Luis", "55555555G", "Hernández", 87));
+        lUsuarios.add(new Usuario("Lucía Sofía", "66666666H", "Sánchez", 15));
+        lUsuarios.add(new Usuario("Tomás Juan", "77777777I", "García", 30));
+        lUsuarios.add(new Usuario("Eva María", "88888888J", "Martínez", 11));
     }
 
     public UsersRepository() {
@@ -46,7 +46,7 @@ public class UsersRepository {
         lUsuarios.removeIf(u -> u.getDni().equals(dni));
     }
 
-    public void actualizarUsuario(Usuario usuario) {
+    public void actualizarUsuario(@org.jetbrains.annotations.NotNull Usuario usuario) {
         eliminarUsuario(usuario.getDni());
         agregarUsuario(usuario);
     }
