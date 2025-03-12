4c56f496fb101f1bdf21761eeb6bc243b78e0975
gonsl-vn <gonzalo.serrano@viewnext.com>
2025-03-12 14:00:19 +0100

    Delete verison anticuada 2 de P5

--- practica53/practica52/src/main/java/com/example/practica52/CalleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.practica52;
-
-import com.example.practica52.model.Calle;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CalleRepository extends JpaRepository<Calle, Integer> {
-}
