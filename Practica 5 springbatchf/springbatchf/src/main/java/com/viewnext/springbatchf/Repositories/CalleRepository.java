a93835d8a46cbaea63c88651f04029570b1dd794
Javier Arias Rodriguez <javier.arias@viewnext.com>
2025-03-27 10:58:14 +0100

    JAR: Subida practica3

--- Practica 5 springbatchf/springbatchf/src/main/java/com/viewnext/springbatchf/Repositories/CalleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.viewnext.springbatchf.Repositories;
-
-import com.viewnext.springbatchf.model.Calle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CalleRepository extends JpaRepository<Calle, Long> {
-}
